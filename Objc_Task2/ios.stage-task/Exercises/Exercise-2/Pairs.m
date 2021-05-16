4aa94843a7d751be81b30c7e537e46a47019fb38
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2021-05-16 15:42:00 +0300

    feat: implement Pairs method in Ex2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,16 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger result = 0;
+    if(![array count])
+        return 0;
+    for(NSInteger i = 0; i < [array count] - 1; i++){
+        for(NSInteger j = i + 1; j < [array count]; j++){
+            if(abs([array[j] intValue] - [array[i] intValue]) == [number intValue])
+                result++;
+        }
+    }
+    return result;
 }
 
 @end
