9ad32808617b6f5814d0dc581c5c3fa5f9f88c80
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2021-05-16 15:25:50 +0300

    feat: implement IPFormatConverter method in Ex3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,36 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    NSMutableArray *temp = [numbersArray mutableCopy];
+    NSString *result;
+    NSInteger count , i = 0, j = 0;
+    
+    if(numbersArray == nil || ![numbersArray count]){
+        return @"";
+    }else if([numbersArray count] < 5){
+        count = [numbersArray count];
+        j = 4 - [numbersArray count];
+        while(j--){
+            [temp addObject:@0];
+        }
+    }else{
+        count = 4;
+    }
+    
+    while(count--){
+        if([numbersArray[i] intValue] < 0){
+            result = @"Negative numbers are not valid for input.";
+            return result;
+        }else if ([numbersArray[i] intValue] > 255){
+            result = @"The numbers in the input array can be in the range from 0 to 255.";
+            return result;
+        }
+        i++;
+    }
+    
+    result = [NSString stringWithFormat:@"%d.%d.%d.%d", [temp[0] intValue], [temp[1] intValue], [temp[2] intValue], [temp[3] intValue]];
+    
+    return result;
 }
 
 @end
